31094a44470c32c1287df0814d8c86556f181930
Alexandre Barbosa <barbosa.ale@gmail.com>
2023-09-19 12:39:23 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
-function Home(){
-    return(
-        <div>
-            <h1>Olá. Isso aqui é um teste !</h1>
-            <h2>Teste 2</h2>
-        </div>
-    )
+function Home() {
+  return (
+    <div>
+      <h1>Olá. Isso aqui é um teste !</h1>
+      <h2>Teste 2</h2>
+    </div>
+  );
 }
 
-
-
-
-
-export default Home
+export default Home;
